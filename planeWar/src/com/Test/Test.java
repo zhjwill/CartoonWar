e096b7b9ae32d03334e8f7eb7ea08588409630d8
zhjwill <53028792+zhjwill@users.noreply.github.com>
2019-08-27 08:54:22 +0800

    Create Test.java

---
+++ planeWar/src/com/Test/Test.java
@@ -0,0 +1,18 @@
+package com.Test;
+
+import com.client.GameClient;
+
+/**
+* @ClassName: Test
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author neuedu_zhj
+* @date 2019年8月22日 下午4:17:16
+*
+*/
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    new GameClient().lanuchFrame();
+	}
+}
